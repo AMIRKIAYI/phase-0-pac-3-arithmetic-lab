676b201a1999dac92b788794d2b890c441f637a3
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-09 11:28:20 +0300

    complete assigment on arithmetics

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+function add(a, b) {
+    return a + b;
+  }
+  function increment(n){
+    return n +1
+  }
+function subtract(a, b){
+    return a- b;
+
+}
+function decrement(n){
+    return n - 1
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+function multiply(a, b){
+return a * b;
+}
+function divide(a, b){
+    return a / b;
+
+}
+
+
+  
